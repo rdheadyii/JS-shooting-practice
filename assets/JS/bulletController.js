8ded1ef14f218683b949c9af4adb807e8c1d7b41
rdheadyii <rdheadyii@gmail.com>
2023-07-20 19:17:32 -0400

    got bullets firing on screen and made them disappear when off screen no delay between shots need to fix

--- assets/JS/bulletController.js
+++ assets/JS/bulletController.js
@@ -1,8 +1,32 @@
+import Bullet from "./bullet.js";
+
 export default class bulletController {
+    bullets = [];
+    timerTillNextBullet = 0;
+
     constructor(canvas) {
         this.canvas = canvas;
     }
 
-    shoot() {}
-    draw(ctx) {}
+    shoot(x, y, speed, damage, delay) {
+        if (this.timerTillNextBullet <= 0) {
+            this.bullets.push(new Bullet(x, y, speed, damage));
+            this.timerTillNextBullet = delay;
+        }
+
+        this.timerTillNextBullet--;
+    }
+    draw(ctx) {
+        this.bullets.forEach((bullet) => {
+            if (this.isBulletOffScreen(bullet)) {
+                const index = this.bullets.indexOf(bullet);
+                this.bullets.splice(index, 1);
+            }
+            bullet.draw(ctx);
+        });
+    }
+
+    isBulletOffScreen(bullet) {
+        return bullet.y <= -bullet.height;
+    }
 }
