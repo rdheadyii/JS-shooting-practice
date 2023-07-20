5a9841459e4af0882c7b788ba3220a788516ff9d
rdheadyii <rdheadyii@gmail.com>
2023-07-20 12:35:29 -0400

    added event listners and functions for keypress for movement

--- assets/JS/player.js
+++ assets/JS/player.js
@@ -5,6 +5,9 @@ export default class Player {
         this.width = 50;
         this.height = 50;
         this.speed = 4;
+
+        document.addEventListener('keydown', this.keydown);
+        document.addEventListener('keyup', this.keyup);
     }
 
     draw(ctx) {
@@ -12,5 +15,41 @@ export default class Player {
         ctx.strokeRect(this.x, this.y, this.width, this.height)
         ctx.fillStyle = 'black';
         ctx.fillRect(this.x, this.y, this.width, this.height)
+    }
+
+    keydown = (e) => {
+        if (e.code === 'KeyW') {
+            this.upPressed = true;
+        }
+
+        if (e.code === 'KeyS') {
+            this.downPressed = true;
+        }
+
+        if (e.code === 'KeyA') {
+            this.leftPressed = true;
+        }
+
+        if (e.code === 'KeyD') {
+            this.rightPressed = true;
+        }
+    }
+
+    keyup = (e) => {
+        if (e.code === 'KeyW') {
+            this.upPressed = false;
+        }
+
+        if (e.code === 'KeyS') {
+            this.downPressed = false;
+        }
+
+        if (e.code === 'KeyA') {
+            this.leftPressed = false;
+        }
+
+        if (e.code === 'KeyD') {
+            this.rightPressed = false;
+        }
     }
 }
