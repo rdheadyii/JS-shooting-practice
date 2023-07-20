40df89ded22cc2ac7f01f60797ed6278901c6483
rdheadyii <rdheadyii@gmail.com>
2023-07-20 19:56:35 -0400

    got enemies to appear on screen with health showing

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,4 +1,5 @@
 import Player from "./player.js";
+import Enemy from "./enemy.js";
 import BulletController from "./bulletController.js";
 
 const canvas = document.getElementById('game');
@@ -10,12 +11,29 @@ canvas.height = 600;
 const bulletController = new BulletController(canvas);
 const player = new Player(canvas.width/2.2, canvas.height/1.3, bulletController);
 
+const enemies = [
+    new Enemy(50, 20, 'green', 5),
+    new Enemy(150, 20, 'red', 5),
+    new Enemy(250, 20, 'gold', 2),
+    new Enemy(350, 20, 'green', 2),
+    new Enemy(450, 20, 'gold', 10),
+    new Enemy(50, 100, 'green', 5),
+    new Enemy(150, 100, 'red', 5),
+    new Enemy(250, 100, 'gold', 2),
+    new Enemy(350, 100, 'green', 2),
+    new Enemy(450, 100, 'gold', 20),
+
+]
+
 function gameLoop() {
     setCommonStyle();
     ctx.fillStyle = 'black';
     ctx.fillRect(0, 0, canvas.width, canvas.height)
     bulletController.draw(ctx)
     player.draw(ctx);
+    enemies.forEach(enemy => {
+        enemy.draw(ctx);
+    });
 }
 
 function setCommonStyle() {
